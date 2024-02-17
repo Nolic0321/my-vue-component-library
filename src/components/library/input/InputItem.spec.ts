2e3daf8a35b91dc07dbf8c198534dd1dce13be07
Collin Gowdy <collin.gowdy@agenium.ai>
2024-02-17 13:59:13 -0600

    Added testing-library

--- src/components/library/input/InputItem.spec.ts
+++ src/components/library/input/InputItem.spec.ts
@@ -1,6 +1,7 @@
 import {describe, it, expect} from 'vitest';
 import InputItem from '@component-library/input/InputItem.vue'
 import { mount } from '@vue/test-utils'
+import { fireEvent, render, screen } from '@testing-library/vue'
 describe('ButtonItem', () => {
   it('should render properly', ()=>{
     const wrapper = mount(InputItem, { props: { modelValue: 'Hello Vitest' } });
@@ -12,5 +13,13 @@ describe('ButtonItem', () => {
     const wrapper = mount(InputItem, { props: { modelValue: 'Goodbye Vitest' } });
     await wrapper.find('input').setValue('Hello Vitest');
     expect(wrapper.emitted('update:modelValue')).toBeTruthy();
+  });
+
+  it('should update the model value using testing library', async ()=>{
+    render(InputItem, { props: { modelValue: 'Goodbye Vitest' } });
+    screen.queryByText('Goodbye Vitest');
+    const input = screen.getByRole('textbox');
+    await fireEvent.update(input, 'Hello Vitest');
+    screen.queryByText('Hello Vitest');
   });
 });
